9973a1c76743c4ca5df8ad8dd87980497e6774ca
Bartek Jasek <BartekJasek4@gmail.com>
2023-09-12 17:21:24 +0200

    determine 1 pizza price

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,8 @@ const select = {
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log('new Product:', thisProduct);
     }
   
@@ -90,7 +92,7 @@ const select = {
       const thisProduct = this;
   
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector('.product__name');
+      const clickableTrigger = thisProduct.element.querySelector('.product__name'); // eslint-disable-line no-unused-vars
   
       /* START: add an event listener to the clickable trigger on the click event */
       thisProduct.accordionTrigger.addEventListener('click', function (event) {
@@ -109,8 +111,67 @@ const select = {
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+    initOrderForm() {
+      const thisProduct = this;
+      console.log('initOrderForm method');
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder() {
+      const thisProduct = this;
     
-  }
+      // Convert form data to an object structure, e.g., { sauce: ['tomato'], toppings: ['olives', 'redPeppers'] }
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+    
+      // Set the price to the default price
+      let price = thisProduct.data.price;
+    
+      // For every category (param)...
+      for (let paramId in thisProduct.data.params) {
+        // Determine param value, e.g., paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+    
+        // For every option in this category
+        for (let optionId in param.options) {
+          // Determine option value, e.g., optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+    
+          // Check if the option is selected in the form data
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+    
+          // If the option is selected and not the default option, increase the price by the option's price
+          if (optionSelected && !option.default) {
+            price += option.price;
+          }
+    
+          // If the option is not selected and is the default option, decrease the price by the option's price
+          if (!optionSelected && option.default) {
+            price -= option.price;
+          }
+        }
+      }
+    
+      // Update the calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
+    }
+  
 
   const app = { // eslint-disable-line no-unused-vars
     initMenu: function(){
